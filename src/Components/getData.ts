f1accd41623e7795a49f46b24808e616f5adaf79
programmergeek <programergeek@gmail.com>
2021-06-15 20:42:35 +0200

    changed getData function into an async function

--- src/Components/getData.ts
+++ src/Components/getData.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
 import axios from "axios";
 
-export function getData(URL: string, APIKey: string): any {
-  return axios.get(`https://api.unsplash.com/${URL}/?client_id=${APIKey}`);
+export async function getData(URL: string, APIKey: string): Promise<any> {
+  const data = await axios.get(
+    `https://api.unsplash.com/${URL}/?client_id=${APIKey}`
+  );
+  return data;
 }
