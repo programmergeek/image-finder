1e4d442445061acb39ca4a99c7ade698bfc013bf
programmergeek <programergeek@gmail.com>
2021-06-15 20:26:10 +0200

    fixed issues with the test

--- src/tests/getData.test.ts
+++ src/tests/getData.test.ts
@@ -1,7 +1,10 @@
 import { getData } from "../Components";
 
 test("Expected response status: 200", () => {
-  return getData().then((data: { status: "" }) => {
-    expect(data.status).toBe("200");
+  return getData(
+    "/photos/random",
+    "CwzsxgVaUemIgH7gJ2ARE5QES6QqYuKAeBRTkMtQWC0"
+  ).then((data: { status: string }) => {
+    expect(data.status).toBe(200);
   });
 });
