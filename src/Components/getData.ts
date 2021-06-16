84af43262729577a51f6422d708aa5878a9d3fd9
programmergeek <programergeek@gmail.com>
2021-06-16 20:41:31 +0200

    removed API key parameter and explicitly stated the api key

--- src/Components/getData.ts
+++ src/Components/getData.ts
@@ -2,9 +2,9 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import axios from "axios";
 
-export async function getData(URL: string, APIKey: string): Promise<any> {
+export async function getData(URL: string): Promise<any> {
   const data = await axios.get(
-    `https://api.unsplash.com/${URL}/?client_id=${APIKey}`
+    `https://api.unsplash.com/${URL}/?client_id=CwzsxgVaUemIgH7gJ2ARE5QES6QqYuKAeBRTkMtQWC0`
   );
   return data;
 }
