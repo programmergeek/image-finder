89f7f04dc6cc84328a5d33a33d79973b6f9c5e63
programmergeek <programergeek@gmail.com>
2021-06-16 20:46:45 +0200

    removed unneeded parameter

--- src/tests/getData.test.ts
+++ src/tests/getData.test.ts
@@ -1,10 +1,7 @@
 import { getData } from "../Components";
 
 test("Expected response status: 200", () => {
-  return getData(
-    "/photos/random",
-    "CwzsxgVaUemIgH7gJ2ARE5QES6QqYuKAeBRTkMtQWC0"
-  ).then((data: { status: string }) => {
+  return getData("/photos/random").then((data: { status: string }) => {
     expect(data.status).toBe(200);
   });
 });
