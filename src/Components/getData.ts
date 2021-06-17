54d67d8f2b3ab73f7440952d11f454e416936f95
programmergeek <programergeek@gmail.com>
2021-06-17 18:12:47 +0200

    changed the return type

--- src/Components/getData.ts
+++ src/Components/getData.ts
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-export async function getData(URL: string): Promise<any> {
-  const data = await axios.get(
+export async function getData(URL: string): Promise<AxiosResponse<any>> {
+  return await axios.get(
     `https://api.unsplash.com/${URL}/?client_id=CwzsxgVaUemIgH7gJ2ARE5QES6QqYuKAeBRTkMtQWC0`
   );
-  return data;
 }
