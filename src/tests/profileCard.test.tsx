50820be1aff51775506cb95e03e96626e3f08f2d
programmergeek <programergeek@gmail.com>
2021-06-17 21:54:39 +0200

    created more tests

--- src/tests/profileCard.test.tsx
+++ src/tests/profileCard.test.tsx
@@ -1,8 +1,26 @@
 import React from "react";
-import ProfileCard from "../Components";
-import { mount } from "enzyme";
+import ProfileCard from "../Components/profileCard/ProfileCard";
+import { mount, shallow } from "enzyme";
 
-test("Should return: 'Jimmy'", () => {
+/**  Test to make sure that the component is rendered */
+test("Should be true", () => {
+  const component = shallow(<ProfileCard username="jimmydean" />);
+  expect(component).toBeTruthy();
+});
+
+/**
+ * @description
+ * Test to make sure data is received from the API
+ */
+test("Should return: 'Jimmy Dean'", () => {
   const component = mount(<ProfileCard username="jimmydean" />);
-  expect(component.state("firstName")).toBe("Jimmy");
+  expect(component.find(".name").children()).toBe("Jimmy Dean");
+});
+
+/**
+ * Test to see if links open on a new page
+ */
+test("Should return: _blank", () => {
+  const component = shallow(<ProfileCard username="jimmdean" />);
+  expect(component.find("a").props()).toHaveAttribute("target", "_blank");
 });
