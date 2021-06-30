51c991c2c36b86b5b416dde99c7aa16b80c581e7
programmergeek <programergeek@gmail.com>
2021-06-30 10:58:14 +0200

    reorganised tests

--- src/tests/searchBar.test.tsx
+++ src/tests/searchBar.test.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import { SearchBar } from "../Components";
 import { mount } from "enzyme";
 
-describe("SearchBar component", () => {
-  it("should render", () => {
-    const component = mount(<SearchBar />);
-    expect(component).toBeTruthy();
+test("should update context", () => {
+  let searchTerm = "";
+  const mockContext = {
+    lookFor: searchTerm,
+  };
+  const component = mount(<SearchBar />, { context: mockContext });
+  const updateSearchTerm = jest.fn(() => {
+    searchTerm = "people";
   });
+  updateSearchTerm();
+  expect(component.context("lookFor")).toBe("people");
+});
 
-  it("should update context", () => {
-    let searchTerm = "";
-    const mockContext = {
-      lookFor: searchTerm,
-    };
-    const component = mount(<SearchBar />, { context: mockContext });
-    const updateSearchTerm = jest.fn(() => {
-      searchTerm = "people";
-    });
-    updateSearchTerm();
-    expect(component.context("lookFor")).toBe("people");
-  });
+test("should render", () => {
+  const component = mount(<SearchBar />);
+  expect(component).toBeTruthy();
 });
