3fba934a40ed64d6b5c1cd77af879d70728585cd
programmergeek <programergeek@gmail.com>
2021-07-01 12:41:28 +0200

    Created search-bar component

---
+++ src/Components/searchBar/searchBar.tsx
@@ -0,0 +1,21 @@
+import React, { useContext, useState } from "react";
+import { SearchContext } from "../../App";
+
+export const SearchBar: React.FC = () => {
+  const search = useContext(SearchContext);
+  const [state, setState] = useState("");
+  search?.updateContext(state);
+
+  return (
+    <div>
+      <input
+        className="search-bar primary-text"
+        onChange={(e: React.FormEvent<HTMLInputElement>) => {
+          setState(e.currentTarget.value);
+        }}
+        value={state}
+        placeholder="Search..."
+      />
+    </div>
+  );
+};
