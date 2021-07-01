ab3a163ae445c207c937c0459bd754f10d2298be
programmergeek <programergeek@gmail.com>
2021-07-01 12:36:48 +0200

    removed context test

--- src/tests/searchBar.test.tsx
+++ src/tests/searchBar.test.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import { SearchBar } from "../Components";
+import { SearchBar } from "../Components/searchBar/searchBar";
 import { mount } from "enzyme";
 
-test("should update context", () => {
-  let searchTerm = "";
-  const mockContext = {
-    lookFor: searchTerm,
-  };
-  const component = mount(<SearchBar />, { context: mockContext });
-  const updateSearchTerm = jest.fn(() => {
-    searchTerm = "people";
-  });
-  updateSearchTerm();
-  expect(component.context("lookFor")).toBe("people");
-});
+interface SearchContextInterface {
+  lookFor: string;
+  updateContext(input: string): void;
+}
 
 test("should render", () => {
   const component = mount(<SearchBar />);
