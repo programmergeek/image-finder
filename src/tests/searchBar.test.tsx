176a786c7b984e00c536328de2221963706ac209
programmergeek <programergeek@gmail.com>
2021-07-01 12:40:02 +0200

    removed type defintion

--- src/tests/searchBar.test.tsx
+++ src/tests/searchBar.test.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import { SearchBar } from "../Components/searchBar/searchBar";
 import { mount } from "enzyme";
 
-interface SearchContextInterface {
-  lookFor: string;
-  updateContext(input: string): void;
-}
-
 test("should render", () => {
   const component = mount(<SearchBar />);
   expect(component).toBeTruthy();
