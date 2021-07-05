4505b5acbe26b0d4f4f165df6ae49e154a9728ed
programmergeek <programergeek@gmail.com>
2021-07-05 17:04:22 +0200

    implemented lazy loading

--- src/Components/imageGrid/ImageGrid.tsx
+++ src/Components/imageGrid/ImageGrid.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { useImageSearch } from "..";
 
 interface Props {
@@ -6,21 +6,50 @@ interface Props {
 }
 
 export const ImageGrid: React.FC<Props> = ({ searchValue }: Props) => {
-  const [images, isLoading, isLastPage] = useImageSearch(searchValue, 1);
+  const [counter, setCounter] = useState(1);
+  const [images, isLoading, hasMore] = useImageSearch(searchValue, counter);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const observer = useRef<any>();
+  const lastPhotoRef = useCallback(
+    (node) => {
+      if (isLoading) return;
+      if (observer.current) observer.current.disconnect;
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setCounter((prevValue) => prevValue + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [isLoading, hasMore]
+  );
 
   return (
     <div>
       {isLoading
         ? "Loading..."
         : images.map((photo, key) => {
-            return (
-              <img
-                src={photo}
-                key={key}
-                width={350}
-                style={{ backgroundColor: "blue", margin: 15 }}
-              />
-            );
+            if (key === images.length - 1) {
+              return (
+                <img
+                  ref={lastPhotoRef}
+                  src={photo}
+                  key={key}
+                  width={350}
+                  style={{ backgroundColor: "blue", margin: 15 }}
+                />
+              );
+            } else {
+              return (
+                <img
+                  src={photo}
+                  key={key}
+                  width={350}
+                  style={{ backgroundColor: "blue", margin: 15 }}
+                />
+              );
+            }
           })}
     </div>
   );
