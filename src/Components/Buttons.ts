1893d47c90b90204c98a3426a5916c36e3938913
programmergeek <programergeek@gmail.com>
2021-08-26 17:58:58 +0200

    created button component

---
+++ src/Components/Buttons.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const button = styled.button`
+  padding: 12px 16px;
+  font-family: inherit;
+  cursor: pointer;
+  border-style: none;
+  border-radius: 5px;
+  font-size: 16px;
+`;
