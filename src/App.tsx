a81c2c717bfef0e3bfcf822e72733ed4148847b8
programmergeek <programergeek@gmail.com>
2021-08-26 18:23:28 +0200

    imported buttons into main app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import { ImageGrid } from "./Components/imageGrid/ImageGrid";
-import { SearchBarV2 } from "./Components/searchBar/searchBar";
+import { PrimaryButton, SecondaryButton, TertiaryButton } from "./Components";
 
-interface SearchContextInterface {
-  lookFor: string;
-  updateContext(input: string): void;
-}
-
-export const SearchContext = React.createContext<SearchContextInterface>({
-  lookFor: "",
-  updateContext(input: string) {
-    this.lookFor = input;
-  },
-});
-
-function App() {
-  const [testState, setTestState] = useState("");
+const App: React.FC = () => {
   return (
-    <SearchContext.Provider
-      value={{
-        lookFor: "",
-        updateContext(input: string) {
-          this.lookFor = input;
-        },
-      }}
-    >
-      <div className="App">
-        <SearchBarV2 input={testState} updateInput={setTestState} />
-        <ImageGrid searchValue={testState} />
-      </div>
-    </SearchContext.Provider>
+    <div>
+      <PrimaryButton>Primary Button</PrimaryButton>
+      <SecondaryButton> Secondary Button </SecondaryButton>
+      <TertiaryButton> Tertiary Button </TertiaryButton>
+    </div>
   );
-}
+};
 
 export default App;
