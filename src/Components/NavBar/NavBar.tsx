3183c670fb07e8d71c5bb0ef4a9e8f9d1d461920
programmergeek <programergeek@gmail.com>
2021-08-27 14:08:32 +0200

    added elements

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,6 +1,33 @@
 import React from "react";
+import {
+  IconButton,
+  PrimaryButton,
+  SearchBar,
+  SecondaryButton,
+  TertiaryButton,
+} from "..";
 import "./styles.css";
+import search from "./../../assets/Images/search.png";
 
 export const NavBar: React.FC = () => {
-  return <div></div>;
+  return (
+    <div id="nav-bar">
+      <div id="page-button">
+        <TertiaryButton>Home</TertiaryButton>
+        <TertiaryButton>About</TertiaryButton>
+        <TertiaryButton>Discover</TertiaryButton>
+      </div>
+      <div id="search-bar">
+        <SearchBar placeholder="Search..." />
+        <IconButton id="search-button">
+          {" "}
+          <img src={search} alt="" height="20px" width="20px" />{" "}
+        </IconButton>
+      </div>
+      <div id="account-buttons">
+        <PrimaryButton>Login</PrimaryButton>
+        <SecondaryButton>Sign Up</SecondaryButton>
+      </div>
+    </div>
+  );
 };
