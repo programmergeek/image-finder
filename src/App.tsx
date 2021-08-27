2be1f07ba73ec1586abc08e2cca2202ff8071645
programmergeek <programergeek@gmail.com>
2021-08-27 14:11:29 +0200

    created some base styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from "react";
 import "./App.css";
-import {
-  PrimaryButton,
-  IconButton,
-  SecondaryButton,
-  TertiaryButton,
-  SearchBar,
-} from "./Components";
-import Search from "./assets/Images/search.png";
+import { NavBar } from "./Components";
 
 const App: React.FC = () => {
   return (
     <div>
-      <PrimaryButton>Primary Button</PrimaryButton>
-      <SecondaryButton> Secondary Button </SecondaryButton>
-      <TertiaryButton> Tertiary Button </TertiaryButton>
-      <IconButton>
-        <img src={Search} alt="search" height="22" width="22" />
-      </IconButton>
-      <SearchBar placeholder="Search..." />
+      <NavBar />
     </div>
   );
 };
