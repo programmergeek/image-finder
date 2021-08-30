6267abfdb041b9a85c18e39f32a883ac9b331337
programmergeek <programergeek@gmail.com>
2021-08-30 21:14:10 +0200

    enclosed input field in form

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { IconButton, SearchBar, TertiaryButton } from "..";
 import "./styles.css";
 import search from "./../../assets/Images/search.png";
@@ -7,11 +8,16 @@ interface Props {
   onChange?: (value: string) => void;
 }
 
+interface Fields {
+  input: string;
+}
+
 export const NavBar: React.FC<Props> = ({ ...props }: Props) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const { register, handleSubmit } = useForm<Fields>();
 
-  const handleChange = (input: string) => {
-    setSearchTerm(input);
+  const handleChange: SubmitHandler<Fields> = (data) => {
+    setSearchTerm(data.input);
     if (props.onChange) props.onChange(searchTerm);
   };
 
@@ -23,15 +29,17 @@ export const NavBar: React.FC<Props> = ({ ...props }: Props) => {
         <TertiaryButton>Discover</TertiaryButton>
       </div>
       <div id="search-bar">
-        <SearchBar
-          id="nav-search"
-          placeholder="Search..."
-          onChange={(input) => handleChange(input.target.value)}
-        />
-        <IconButton id="search-button">
-          {" "}
-          <img src={search} alt="" height="20px" width="20px" />{" "}
-        </IconButton>
+        <form onSubmit={handleSubmit(handleChange)}>
+          <SearchBar
+            id="nav-search"
+            placeholder="Search..."
+            {...register("input")}
+          />
+          <IconButton id="search-button" type="submit">
+            {" "}
+            <img src={search} alt="" height="20px" width="20px" />{" "}
+          </IconButton>
+        </form>
       </div>
     </div>
   );
