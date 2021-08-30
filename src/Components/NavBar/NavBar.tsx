482ddab5d6ba7129bbb26ac2ba74bc70f771382a
programmergeek <programergeek@gmail.com>
2021-08-30 22:56:57 +0200

    implemented useEffect hook to update internal state

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { IconButton, SearchBar, TertiaryButton } from "..";
 import "./styles.css";
@@ -21,6 +21,11 @@ export const NavBar: React.FC<Props> = ({ ...props }: Props) => {
     if (props.onChange) props.onChange(data.input);
   };
 
+  useEffect(() => {
+    setSearchTerm(watch("input"));
+    if (props.onChange) props.onChange(watch("input"));
+  });
+
   return (
     <div id="nav-bar">
       <div id="page-button">
@@ -29,13 +34,7 @@ export const NavBar: React.FC<Props> = ({ ...props }: Props) => {
         <TertiaryButton>Discover</TertiaryButton>
       </div>
       <div id="search-bar">
-        <form
-          onSubmit={handleSubmit(handleChange)}
-          onChange={() => {
-            setSearchTerm(watch("input"));
-            if (props.onChange) props.onChange(watch("input"));
-          }}
-        >
+        <form onSubmit={handleSubmit(handleChange)}>
           <SearchBar
             id="nav-search"
             placeholder="Search..."
