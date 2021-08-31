65d12d1ea184e1d9afc681aeb07fe5cb19de5df9
programmergeek <programergeek@gmail.com>
2021-08-31 19:53:31 +0200

    created internal states defined return values

--- src/Hooks/useSearch.ts
+++ src/Hooks/useSearch.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { AxiosResponse } from "axios";
 import { useState } from "react";
 
 interface Props {
@@ -6,5 +8,9 @@ interface Props {
 }
 
 export const useSearch = (params: Props) => {
-  return;
+  const [data, setData] = useState<AxiosResponse>({} as AxiosResponse);
+  const [query, setQuery] = useState(params.query);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  return { data, setQuery, errorMessage };
 };
