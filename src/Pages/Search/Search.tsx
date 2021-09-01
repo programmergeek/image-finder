39cea44a0f2c221332052fb04308bed3d5663834
programmergeek <programergeek@gmail.com>
2021-09-01 07:31:05 +0200

    created method to process data from api

--- src/Pages/Search/Search.tsx
+++ src/Pages/Search/Search.tsx
@@ -1,7 +1,30 @@
+import { AxiosResponse } from "axios";
 import React, { useState } from "react";
 import { NavBar } from "../../Components";
 import { useSearch } from "../../Hooks";
 import "./styles.css";
+
+interface ProcessedData {
+  photos: {
+    id: string;
+    description: string;
+    photoUrl: string;
+  }[];
+}
+
+const processData = (data: AxiosResponse) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const processedData: ProcessedData = data.data.results.map(
+    (photo: Record<string, any>) => {
+      return {
+        id: photo.id,
+        description: photo.description,
+        photoUrl: photo.urls.regular,
+      };
+    }
+  );
+  return processedData;
+};
 
 export const Search: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
