1b5be777756cb906a3dd0a1d7766dfe1186d031c
programmergeek <programergeek@gmail.com>
2021-09-08 23:57:13 +0200

    created form

--- src/Pages/Sign Up/SignUp.tsx
+++ src/Pages/Sign Up/SignUp.tsx
@@ -1,7 +1,93 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import {
+  TextField,
+  PrimaryButton,
+  TertiaryButton,
+  IconButton,
+} from "./../../Components";
+import facebook from "./../../assets/Images/facebook.png";
+import google from "./../../assets/Images/google.png";
+import twitter from "./../../assets/Images/twitter.png";
 import "./styles.css";
 
+interface Fields {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
 export const SignUp: React.FC = () => {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Fields>();
+
+  return (
+    <div className="form-container">
+      <div className="page-art"></div>
+      <div className="form">
+        <p className="auth-header">Sign Up</p>
+        <form>
+          <div id="name">
+            <div className="field sub-field">
+              <label>First Name</label>
+              <TextField
+                className="input-field"
+                {...register("firstName", { required: true })}
+              />
+            </div>
+            <div className="field sub-field">
+              <label>Last Name</label>
+              <TextField
+                className="input-field"
+                {...register("lastName", { required: true })}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label>Username</label>
+            <TextField
+              className="input-field"
+              {...register("username", { required: true })}
+            />
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <TextField
+              className="input-field"
+              {...register("email", { required: true })}
+            />
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <TextField
+              className="input-field"
+              {...register("password", { required: true })}
+              type="password"
+            />
+          </div>
+          <PrimaryButton type="submit" className="primary-form-btn">
+            Sign Up
+          </PrimaryButton>
+        </form>
+        <TertiaryButton>Login</TertiaryButton>
+        <p className="muted">Sign up with</p>
+        <div className="social-media-auth">
+          <IconButton className="media">
+            <img src={google} alt="google" className="media-icon" />
+          </IconButton>
+          <IconButton className="media">
+            <img src={facebook} alt="facebook" className="media-icon" />
+          </IconButton>
+          <IconButton className="media">
+            <img src={twitter} alt="twitter" className="media-icon" />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 };
