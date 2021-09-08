b321e5a408332369a8c63dacacc45731952285ff
programmergeek <programergeek@gmail.com>
2021-09-08 14:19:18 +0200

    created text field component

--- src/Components/Inputs.ts
+++ src/Components/Inputs.ts
@@ -1,11 +1,20 @@
 import styled from "styled-components";
 
-export const SearchBar = styled.input`
+const Input = styled.input`
   height: 50px;
-  width: 60%;
   font-family: inherit;
+  font-size: 18px;
+`;
+
+export const SearchBar = styled(Input)`
+  width: 60%;
   padding-left: 25px;
   border-radius: 50px;
   border: 2px solid #bababa;
-  font-size: 18px;
+`;
+
+export const TextField = styled(Input)`
+  border: 2px solid black;
+  border-radius: 3px;
+  padding: 10px;
 `;
